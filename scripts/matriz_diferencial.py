c4a0682ddc1023c931e56c5854fff9c999c9c9b1
RomanElGuapo <angelzl@lcg.unam.mx>
2024-12-03 09:47:01 -0600

    Script completo matriz_diferencial.py

--- scripts/matriz_diferencial.py
+++ scripts/matriz_diferencial.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import seaborn as sb
+import numpy as np
+from scipy import stats
 
-NH4_1 = pd.read_excel('../data/EANpec1_NH4_N2/GSM1573196_Batch_1_EAN1pec_NH4.xlsx')
-N2_1 = pd.read_excel('../data/EANpec1_NH4_N2/GSM1573197_Batch_1_EAN1pec_N2.xlsx')
+NH4_1 = pd.read_excel('GSM1573196_Batch_1_EAN1pec_NH4.xlsx')
+N2_1 = pd.read_excel('GSM1573197_Batch_1_EAN1pec_N2.xlsx')
 
-NH4_1['RPKM'] - N2_1['RPKM']
+#Se rearregla a dos dimensiones el vector de RPKMs de NH4
+#a dos dimensiones para después restarle cada uno de los
+#valores de N2
+
+Matriz_diferencial = NH4_1['RPKM'].values[:,None] - N2_1['RPKM'].values
+
+sb.heatmap(data = Matriz_diferencial,cmap = 'vlag')
